cfd3ae2d385a5ada8f26444f358210118e5444d3
Akiyamov <116005851+Akiyamov@users.noreply.github.com>
2023-03-16 18:39:59 +0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for name in *.mp4; do
+	palette="/tmp/palette.png"
+	filters="fps=15,scale=-1:720:flags=lanczos"
+
+	ffmpeg -v warning -i $name -vf "$filters,palettegen" -y $palette
+	ffmpeg -v warning -i $name -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y "${name%.*}.gif"
+done
