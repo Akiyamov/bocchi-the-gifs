28d27813b2841b4cb52c8db6e077effad0c8cbb6
Akiyamov <AkiyamaSS@yandex.ru>
2023-03-16 23:00:12 +0500

    test [no ci]

--- script-360.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for name in *.mp4; do
-	palette="/tmp/palette.png"
-	filters="fps=15,scale=640:-1:flags=lanczos"
-
-	ffmpeg -v warning -i $name -vf "$filters,palettegen" -y $palette
-	ffmpeg -v warning -i $name -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y "${name%.*}.gif"
-done
